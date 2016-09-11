90944099970de060099d421afe35a34cb1b103d6
kmne68 <kmne68@gmail.com>
2016-09-10 22:54:47 -0500

    Create BankAccount class and tests.

---
+++ src/selectedtutorials/BankAccount.java
@@ -0,0 +1,104 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package selectedtutorials;
+
+import java.text.DecimalFormat;
+
+/**
+ *
+ * @author Keith
+ */
+public class BankAccount {
+    
+    private int accountNumber;
+    private double accountBalance;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    
+    private static DecimalFormat df2 = new DecimalFormat(".##");
+    
+    public double addFunds(double deposit) {
+        
+        System.out.println("Your account balance is $" + df2.format(accountBalance));
+        return accountBalance += deposit;
+    }
+    
+    
+    public double withdrawFunds(double withdrawalAmount) {
+        
+        if (accountBalance >= withdrawalAmount)
+            accountBalance -= withdrawalAmount;
+        else 
+            System.out.println("Insufficient funds.");
+        return accountBalance;
+    }
+    
+    
+    public void setAccountNumber(String account) {
+        
+        this.accountNumber = accountNumber;
+    }
+    
+    
+    public String printBalance() {
+        
+        return df2.format(accountBalance);
+    }
+    
+    
+    public void setBalance (double balance) {
+        
+        this.accountBalance = balance;
+    }
+    
+    
+    public void setFirstName (String nameFirst) {
+        
+        this.firstName = nameFirst;
+    }
+    
+    
+    public void setLastName (String nameLast) {
+        
+        this.lastName = nameLast;
+    }
+    
+    
+    public void setPhone (String phone) {
+        
+        this.phoneNumber = phone;
+    }
+    
+    
+    public int getAccountNumber() {
+        
+        return accountNumber;
+    }
+    
+    
+    public double getBalance() {
+        
+        return accountBalance;
+    }
+    
+    
+    public String getFirstName() {
+        
+        return firstName;
+    }
+    
+    
+    public String getLastName() {
+        
+        return lastName;
+    }
+    
+    
+    public String getPhone() {
+        
+        return phoneNumber;
+    }
+}
--- src/selectedtutorials/SelectedTutorials.java
+++ src/selectedtutorials/SelectedTutorials.java
@@ -14,6 +14,23 @@ public class SelectedTutorials {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        BankAccount account = new BankAccount();
+        
+        account.setAccountNumber("12345");
+        account.setBalance(538.97);
+        account.setLastName("John");
+        account.setFirstName("John");
+        account.setPhone("555-555-5555");
+        
+        account.addFunds(37.04);
+        account.withdrawFunds(598.02);
+        System.out.println("Your account balance is $" + account.getBalance());
+        
+        account.withdrawFunds(17.31);
+        System.out.println("Your account balance is $" + account.printBalance());
+        account.withdrawFunds(558.69);
+        System.out.println("Your account balance is $" + account.printBalance());
+        
     }
 }
