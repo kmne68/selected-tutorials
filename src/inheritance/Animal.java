340453bb386d5324b0f5c2eb33e2ad4f5c6892db
kmne68 <kmne68@gmail.com>
2016-09-13 00:15:03 -0500

    Add package 'inheritance'.

---
+++ src/inheritance/Animal.java
@@ -0,0 +1,73 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritance;
+
+/**
+ *
+ * @author Keith
+ */
+public class Animal {
+    
+    private String name;
+    private int brain;
+    private int body;
+    private int size;
+    private int weight;
+    
+    public Animal(String name, int brain, int body, int size, int weight) {
+        
+        this.name = name;
+        this.brain = brain;
+        this.body = body;
+        this.size = size;
+        this.weight = weight;
+    }
+
+    
+    public void eat() {
+        
+    }
+    
+    
+    public void move() {
+        
+    }
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the brain
+     */
+    public int getBrain() {
+        return brain;
+    }
+
+    /**
+     * @return the body
+     */
+    public int getBody() {
+        return body;
+    }
+
+    /**
+     * @return the size
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * @return the weight
+     */
+    public int getWeight() {
+        return weight;
+    }
+    
+    
+}
---
+++ src/inheritance/Dog.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritance;
+
+/**
+ *
+ * @author Keith
+ */
+public class Dog extends Animal {
+    
+    private int eyes;
+    private int legs;
+    private int title;
+    private int teeth;
+    private String coat;
+    
+    public Dog(String name, int brain, int body, int size, int weight) {
+        
+        super(name, brain, body, size, weight);
+        
+        
+    }
+    
+}
