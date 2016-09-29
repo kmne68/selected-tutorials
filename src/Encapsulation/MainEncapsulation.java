5159c7d82b1b4d121bf198f3f4654a0737d8259a
kmne68 <kmne68@gmail.com>
2016-09-29 00:13:05 -0500

    Add enhanced player with encapsulation.

--- src/Encapsulation/MainEncapsulation.java
+++ src/Encapsulation/MainEncapsulation.java
@@ -20,5 +20,13 @@ public class MainEncapsulation {
         int damage = 10;
         player.loseHealth(damage);
         System.out.println("Remaining health = " + player.healthRemaining());
+        
+        damage = 11;
+        player.health = 100;
+        player.loseHealth(damage);
+        System.out.println("Remaining health = " + player.healthRemaining());
+        
+        EnhancedPlayer enhancedPlayer = new EnhancedPlayer("Keith", 200, "Sword");
+        System.out.println("Player " + enhancedPlayer.getName() + " has remaining health of " + enhancedPlayer.getHealth());
     }
 }
