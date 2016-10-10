8052f893d5a765027576bca5de5d1c69aefc2eb2
kmne68 <kmne68@gmail.com>
2016-10-09 23:49:03 -0500

    Add car subclasses.

---
+++ src/Car/FordRanger.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Car;
+
+/**
+ *
+ * @author Keith
+ */
+public class FordRanger extends Car {
+    
+    public FordRanger() {
+        
+        super("Rick", 4, 4, true);
+    }
+}
---
+++ src/Car/ModelT.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Car;
+
+/**
+ *
+ * @author Keith
+ */
+public class ModelT extends Car {
+    
+    public ModelT() {
+        
+        super("Blackie", 4, 4, true);
+    }
+        
+}
