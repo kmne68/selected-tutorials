387e817e4f4898614b17e1baa6b275f36fcf6872
kmne68 <kmne68@gmail.com>
2016-10-30 03:04:34 -0500

    Add ArrayListDemo

---
+++ src/ArrayListDemo/ArrayListDemo.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ArrayListDemo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Keith
+ */
+public class ArrayListDemo {
+    
+    public static void main(String[] args) {
+        
+        
+    }
+    
+    private ArrayList<String> groceryList = new ArrayList<String>();
+    
+    public void addGroceryItem(String item) {
+        
+        groceryList.add(item);
+    }
+    
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your grocery list");
+        
+        for(int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i+1) + ". " + groceryList.get(i));
+        }
+    }
+    
+    public void modifyGroceryItem(int position, String newItem) {
+        
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position + 1) + " has been modified.");
+    }
+    
+    
+    public void removeGorceryItem(int position) {
+        
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+    }
+}
