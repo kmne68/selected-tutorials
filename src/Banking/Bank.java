65fcb2d0ffd5873b70038a0278252e937c4b8902
kmne68 <kmne68@gmail.com>
2016-11-20 01:59:17 -0600

    Create Bank, Branch and Customer classes;

---
+++ src/Banking/Bank.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Banking;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Keith
+ */
+public class Bank {
+    
+    private ArrayList<Branch> branchesList = new ArrayList<>();
+    
+    public Bank(Customer customer) {
+        
+    }
+}
---
+++ src/Banking/Branch.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Banking;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Keith
+ */
+public class Branch {
+    
+    private String name;
+    private ArrayList<Customer> customers;
+    
+    public Branch(String name) {
+        
+        this.name = name;
+        this.customers = new ArrayList<Customer>();
+    }
+    
+    
+    public String getName() {
+        
+        return name;
+    }
+    
+    
+    public boolean newCustomer(String customerName, double initialAmount) {
+        
+        if(findCustomer(customerName) == null) {
+            this.customers.add(new Customer(customerName, initialAmount));
+            return true;
+        }
+        return false;
+    }
+    
+    
+    public boolean addCustomerTransaction(String customerName, double amount) {
+        
+        Customer existingCustomer = findCustomer(customerName);
+        
+        if(existingCustomer != null) {
+            
+            existingCustomer.addTransaction(amount);
+            return true;
+        }
+        return false;
+    }
+    
+    
+    private Customer findCustomer(String name) {
+        
+        for(int i = 0; i < this.customers.size(); i++) {
+            Customer checkedCustomer = this.customers.get(i);
+            if(checkedCustomer.getName().equals(name)) {
+                return checkedCustomer;
+            }
+        }
+        return null;
+    }
+}
