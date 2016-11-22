d938fa9e11a753598ae5143180fea8c04ae05af9
kmne68 <kmne68@gmail.com>
2016-11-21 23:43:31 -0600

    Add bank class details

--- src/Banking/Bank.java
+++ src/Banking/Bank.java
@@ -12,9 +12,60 @@ import java.util.ArrayList;
  */
 public class Bank {
     
-    private ArrayList<Branch> branchesList = new ArrayList<>();
+    String name;
+    private ArrayList<Branch> branchesList;
     
-    public Bank(Customer customer) {
-        
+    public Bank(String name) {
+
+        this.name = name;
+        this.branchesList = new ArrayList<>();
     }
+    
+    public boolean addBranch(String branchName) {
+        
+        if(findBranch(branchName) == null) {
+            this.branchesList.add(new Branch(branchName));
+        }
+        return false;
+    }
+    
+    public boolean addCustomer(String branchName, String customerName, double initialAmount) {
+        
+        Branch branch = findBranch(branchName);
+        if(branch != null) {
+            return branch.newCustomer(customerName, initialAmount);
+        }
+        return false;
+    }
+    
+    
+    public boolean addCustomerTransaction(String branchName, String customerName, double amount) {
+        
+        Branch branch = findBranch(branchName);
+        if(branch != null) {
+            return branch.addCustomerTransaction(customerName, amount);
+        }
+        return false;
+    }
+    
+    public Branch findBranch(String branchName) {
+        for(int i = 0; i < this.branchesList.size(); i++) {
+            Branch checkedBranch = this.branchesList.get(i);
+       
+            if(checkedBranch.getName().equals(branchName)) {
+                return checkedBranch;
+            }
+        }
+        return null;
+    }
+    
+    public boolean listCustomers(String branchName, boolean showTransaction) {
+        
+        Branch branch = findBranch(branchName);
+        if(branch != null) {
+            System.out.println("Customer details for branch " + branch.getName);
+        }
+    }
+    
+    
 }
