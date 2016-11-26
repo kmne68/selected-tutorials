34eb6835f672162e8e69a5635cc285aa7349b746
kmne68 <kmne68@gmail.com>
2016-11-25 23:55:09 -0600

    Add pre-linked list code.

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -4,6 +4,8 @@
  */
 package LinkedList;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Keith
@@ -12,7 +14,34 @@ public class Main {
     
     public static void main (String[] args) {
         
+        Customer firstCustomer = new Customer("Keith", 77.93);
+        System.out.println("Balance for firstCustomer: " + firstCustomer.getBalance());
+        Customer secondCustomer = new Customer("Kelen", 47.32);
+        System.out.println("Balance for secondCustomer: " + secondCustomer.getBalance());
+        Customer thirdCustomer;
+        thirdCustomer = secondCustomer;
+        System.out.println("Balance for thirdCustomer: " + thirdCustomer.getBalance());
         
+        thirdCustomer.setBalance(12.18);
+        System.out.println("Balance for thirdCustomer: " + thirdCustomer.getBalance());
+        
+        System.out.println("Balance for secondCustomer: " + secondCustomer.getBalance());
+        
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+        
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+        
+        for (int i = 0; i < intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));            
+        }
+        
+        intList.add(1, 2);
+        
+        for (int i = 0; i < intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));            
+        }        
     }
     
 }
