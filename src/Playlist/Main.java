5e0c59f4317038d97fdfc27cc3f08e6efa4ce3ce
kmne68 <kmne68@gmail.com>
2016-12-09 23:18:54 -0600

    Complete main class.

--- src/Playlist/Main.java
+++ src/Playlist/Main.java
@@ -5,6 +5,7 @@
 package Playlist;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
 import java.util.Scanner;
@@ -117,6 +118,21 @@ public class Main {
                     }
                     break;
                 case 3:
+                    if(forward) {
+                        if(listIterator.hasPrevious()) {
+                            System.out.println("Now replaying " + listIterator.previous().toString());
+                            forward = false;
+                        } else {
+                            System.out.println("We are at the start of the list.");
+                        }
+                    } else {
+                        if(listIterator.hasNext()) {
+                            System.out.println("Now replaying " + listIterator.next().toString());
+                            forward = true;
+                        } else {
+                            System.out.println("We have reached the end of the list.");
+                        }
+                    }
                     break;
                 case 4:
                     printList(playList);
@@ -124,15 +140,20 @@ public class Main {
                 case 5:
                     printMenu();
                     break;
+                case 6:
+                    if(playList.size()> 0) {
+                        listIterator.remove();
+                        if(listIterator.hasNext()) {
+                            System.out.println("Now playing " + listIterator.next());
+                        } else if (listIterator.hasPrevious()) {
+                            System.out.println("Now playing " + listIterator.previous());
+                        }
+                            
+                    }
             }
         }
     }
     
-    
-    private static void printList(LinkedList playList) {
-        
-        
-    }
     
     
     private static void printMenu() {
@@ -143,6 +164,20 @@ public class Main {
                 "2 - to play previous song\n" +
                 "3 - to replay the current song\n" +
                 "4 - list songs in the playlist\n" +
-                "5 - print available actions");
+                "5 - print available actions" + 
+                "6 - delete current song from playlist");
     }
+    
+    
+    private static void printList(LinkedList<Song> playList) {
+        
+        Iterator<Song> iterator = playList.iterator();
+        System.out.println("==============================");
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
+        System.out.println("==============================");
+            
+        }
+    }
+    
 }
