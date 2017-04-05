a30c15812e9e563eb1d88273de8e29b048532339
kmne68 <kmne68@gmail.com>
2017-04-04 23:54:58 -0500

    Initial commit for interface exercise.

---
+++ src/InterfaceExercise/Monster.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InterfaceExercise;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Keith
+ */
+public class Monster implements Savable {
+
+    @Override
+    public ArrayList savedValues() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void populateFields(ArrayList values) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/InterfaceExercise/Savable.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InterfaceExercise;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Keith
+ */
+public interface Savable {
+    
+    public ArrayList savedValues();
+    public void populateFields(ArrayList values);
+}
