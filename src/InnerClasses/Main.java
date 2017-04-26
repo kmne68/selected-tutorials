9cbbe00ecc35527e4ba8b99f9d327cbffd44782a
kmne68 <kmne68@gmail.com>
2017-04-26 00:09:37 -0500

    Add methods to Gearbox

--- src/InnerClasses/Main.java
+++ src/InnerClasses/Main.java
@@ -13,10 +13,7 @@ public class Main {
     
     public static void main(String[] args) {
         Gearbox mcLaren = new Gearbox(6); // outer class
-        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
-//        Gearbox.Gear second = new Gearbox.Gear(2, 15.4);
-  //      Gearbox.Gear third = new mcLaren.Gear(3, 17.8);
-        
-        System.out.println(first.driveSpeed(1000));
+        mcLaren.addGear(1, 5.3);
+
     }
 }
