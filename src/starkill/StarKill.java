11ce5a8589a4938e1e1350076edf3c15b3aa98ac
Keith Emery <kmne68@gmail.com>
2018-04-17 14:32:08 -0500

    Complete starkill with correct logic

--- src/starkill/StarKill.java
+++ src/starkill/StarKill.java
@@ -14,9 +14,13 @@ public class StarKill {
     public static void main(String[] args) {
         
         System.out.println("123*456 --> " + starKill("123*456"));
-  //      System.out.println("*123*456 --> " + starKill("*123*456"));
+        System.out.println("*123*456 --> " + starKill("*123*456"));
         System.out.println("*123**456 --> " + starKill("123**456"));
         System.out.println("wacy*xko --> " + starKill("wacy*xko"));
+        System.out.println("*str*in*gy --> " + starKill("*str*in*gy"));
+        System.out.println("a*b --> " + starKill("a*b"));        
+        System.out.println("******aabbc*** --> " + starKill("******aabbc***"));
+
         
         // need to add code to deal with asterisk in first position
     }
@@ -26,16 +30,17 @@ public class StarKill {
         
         String result = "";
         
-        for(int character = 0; character < start.length(); character++) {
+        for(int i = 0; i < start.length(); i++) {
             
-            if(start.charAt(character) != '*') {
-                result += start.charAt(character);
-            } else {
-                
-                result = result.substring(0, character - 1);
-                character++;
+            if(i == 0 && start.charAt(i) != '*') {
+                result += start.charAt(i);
+            } 
+            if (i > 0 && start.charAt(i) != '*' && start.charAt(i - 1) != '*') {                
+                result += start.charAt(i);
             }
-            
+            if (i > 0 && start.charAt(i) == '*' && start.charAt(i - 1) != '*') {
+                result = result.substring(0, result.length() - 1);
+            }        
             
         }
         
